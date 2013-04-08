1fe2e3d3628df8071d92d6d6b1451793b7655453
Ashwin Lachman <ashwin.lachman@gmail.com>
2013-04-08 18:51:46 -0400

    commit changes with logic seeding

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,5 @@
 class Book < ActiveRecord::Base
-  attr_accessible :author, :bookDescription, :bookTitle, :category, :dueDate, :isbn, :lateFee, :onHold, :overDue, :signOutDate, :userName
-
+  attr_accessible :author, :userName, :bookDescription, :bookTitle, :category, :dueDate, :isbn, :lateFee, :onHold, :overDue, :signOutDate
 def checkdate
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Book.create(bookTitle: 'The Host', author: 'Stephanie Mayor', bookDescription: 'A very prominent society host.', isbn: 2093943935, userName: ' ')
+Book.create(bookTitle: 'Harry Potter', author: 'Ian Smith', bookDescription: 'The adventures of Harry.', isbn: 2093943535, userName: ' ')
+Book.create(bookTitle: 'Fundamentals of Rails', author: 'James Hoger', bookDescription: 'All about Rails.', isbn: 2093953935, userName: ' ')
+Book.create(bookTitle: 'Hosting Insights', author: 'Blinstein Burgur', bookDescription: 'Everything you need to know about hosting.', isbn: 2093643935, userName: ' ')
+Book.create(bookTitle: 'Future of IT', author: 'Smith Barnes', bookDescription: 'Where is IT headed?', isbn: 2092343935, userName: ' ')
+Book.create(bookTitle: 'What is next', author: 'Bloom Garner', bookDescription: 'The next steps in the computer age.', isbn: 2093947535, userName: ' ')
