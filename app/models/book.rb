fee30689c277b165e22cf49fc2446453ac3ce701
Ashwin Lachman <ashwin.lachman@gmail.com>
2013-04-08 22:13:18 -0400

    change ui

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ActiveRecord::Base
   attr_accessible :author, :userName, :bookDescription, :bookTitle, :category, :dueDate, :isbn, :lateFee, :onHold, :overDue, :signOutDate
-def checkdate
-  
+def checkIn 
+  Book.userName = ""
 end
 
 end
--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -2,3 +2,5 @@ class Dashboard < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :index,:bookTitle,:author,:isbn,:userName,:signOutDate, :dueDate, :lateFee,:onHold,:overDue
 end
+
+
